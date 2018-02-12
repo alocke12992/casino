3a7b94933ade5fefbf66574dacb0555ae2a98aae
Andrew Locke <alocke12992@gmail.com>
2018-02-12 15:19:06 -0700

    fix merge conflict

--- roulette.rb
+++ roulette.rb
@@ -3,15 +3,8 @@ require_relative 'roulette_table'
 class Roulette
   attr_accessor :colors, :numbers
 
-<<<<<<< HEAD
   def initialize
-=======
-<<<<<<< HEAD
-  def initialize 
-=======
-  def initialize
->>>>>>> git
->>>>>>> finish
+
     @colors = %w(red black)
     @numbers = %w(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
     @result = []
@@ -44,9 +37,6 @@ class Roulette
 end
 
 roulette = Roulette.new
-<<<<<<< HEAD
 roulette.run
 
-=======
-roulette.generate_result
->>>>>>> finish
+
