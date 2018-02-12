c600e3f5a01b2b4a2f53854780f32f957416d448
Shandelle Lovato <ShandelleLovato@gmail.com>
2018-02-12 14:21:50 -0700

    creates dice.rb, which can roll X amt of dice, and puts results

--- dice.rb
+++ dice.rb
@@ -1,1 +1,32 @@
-# Do dice stuff here
+class Dice
+  attr_accessor
+
+  def initialize
+    @dice = rand(1..6)
+  end
+
+  def roll_dice
+    puts @dice
+  end
+
+end
+
+puts "How many dice?"
+@num = gets.strip.to_i
+
+
+def rolling
+  number = 0
+  until number == @num
+    @dice_roll = Dice.new
+    @total = []
+    @total << @dice_roll.roll_dice
+
+    number += 1
+  end
+  # @total.join(' ')
+end
+
+puts
+puts "You rolled:"
+rolling
