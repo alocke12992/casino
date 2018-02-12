4de07c11edec24500683408bd663de2cc07cf8d0
Andrew Locke <alocke12992@gmail.com>
2018-02-12 14:58:24 -0700

    rebase

--- roulette.rb
+++ roulette.rb
@@ -3,7 +3,7 @@ require_relative 'roulette_table'
 class Roulette
   attr_accessor :colors, :numbers
 
-  def initialize 
+  def initialize
     @colors = %w(red black)
     @numbers = %w(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
     @result = []
