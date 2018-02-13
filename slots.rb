c16d6aa6d0558432bfbfa5c0db38a941884f9a80
Andrew Locke <alocke12992@gmail.com>
2018-02-12 17:52:36 -0700

    Adds working slots

--- slots.rb
+++ slots.rb
@@ -1,5 +1,6 @@
-
+# require_relative 'slot_machine'
 require 'pry'
+require 'colorize'
 
 class Slots
   attr_reader :player, :casino
@@ -7,14 +8,16 @@ class Slots
   def initialize(player, casino)
     @player = player
     @casino = casino
+
   end
+
 
   def play_game
     puts '1) Play Game'
     puts '2) exit to casino'
     choice = gets.to_i
     if choice == 1
-      play_game 
+      welcome 
     elsif choice == 2
       @casino.menu
     else 
@@ -23,20 +26,60 @@ class Slots
     end
   end
 
-  def make_bet
-    puts "What would you like to bet?"
-    user_bet = gets.strip.to_i 
-    @bet = user_bet 
-    @player.wallet -= @bet
-    puts "your bet was #{@bet}" 
-    puts "your wallet is now #{@player.wallet}"
-    binding.pry 
-  end 
-
-  def play_game 
+  def  welcome
     puts "first you must make a bet"
     make_bet
   end 
 
+  def make_bet
+    puts "What would you like to bet?"
+    user_bet = gets.strip.to_i 
+    @user_bet = user_bet 
+    @player.wallet -= @user_bet
+    puts "your bet was #{@user_bet}" 
+    puts "your wallet is now #{@player.wallet}"
+    run_slots
+  end 
+
+
+
+  def run_slots 
+    @slot_A = [1,2,3,4,5]
+    @slot_B = [1,2,3,4,5]
+    @slot_C = [1,2,3,4,5]
+    @a = 0
+    @b = 0
+    @c = 0
+    @i = 0
+    while @i < 10
+        @a = @slot_A.sample.to_i
+        @b = @slot_B.sample.to_i
+        @c = @slot_C.sample.to_i
+        puts "#{@a} #{@b} #{@c}".colorize(:green)
+        puts "#{@a} #{@b} #{@c}".colorize(:red)
+        puts "#{@a} #{@b} #{@c}".colorize(:blue)
+        @i += 1
+    end 
+        prize(@user_bet) 
+    end  
+
+  def prize(bet) 
+    if @a == @b || @a == @c || @b == @c 
+      @user_bet += bet * 2 
+      puts "WINNER!"
+      puts @user_bet
+    elsif @a == @b && @a == @c
+      @user_bet += bet * 5
+      puts "JackPot!"
+      puts @user_bet
+    else
+      puts "You Lose, Better Luck Next Time"
+      @user_bet = 0 
+    end
+    # puts "Your initial bet was: $".colorize(:green)
+    puts  "You currently have $#{@user_bet} to play with".colorize(:green)
+  play_game
+  end
+
 end 
- 
+  
